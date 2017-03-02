3a36c01c9b2b63b37739bb565b2a92b9ed45c807
Андрей <Gaidargee_andrey@mail.ru>
2017-03-02 18:42:53 +0300

    Initial commit

---
+++ lab2.js
@@ -0,0 +1,21 @@
+function tree(el) {
+    if (el == document.documentElement) $("#result").empty();
+    while (el) {
+        if (1 == el.nodeType && el) {
+            var i = 0;
+            elem = el;
+            while (elem) {
+                i++;
+                elem = elem.parentNode;
+            }
+            i -= 2;
+            for (var j = 0; j < i; j++) {
+                $("#result").append('+');
+            }
+            $("#result").append(el.tagName);
+            $("#result").append('\n');
+        }
+        tree(el.firstChild);
+        el = el.nextSibling;
+    }
+}
